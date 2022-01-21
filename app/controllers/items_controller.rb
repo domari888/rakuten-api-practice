c9d34e2c981f8e49abd170099299d113166c1796
damari <motokin128@gmail.com>
2022-01-22 02:15:28 +0900

    add: マイアイテムの追加機能を実装
    検索結果の各アイテムの列にマイアイテム追加用のボタンを設定して押下後はマイアイテムとしてデータベースに保存・マイページへリダイレクトとする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,9 @@ class ItemsController < ApplicationController
       @items = RakutenWebService::Ichiba::Product.search(keyword: params[:keyword])
     end
   end
+
+  def create
+    current_user.items.create!(name: params[:name], genre: params[:genre], image: params[:image])
+    redirect_to user_path(current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#search'
+  resource :items, only: %i[search create]
   resources :users, only: %i[show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
